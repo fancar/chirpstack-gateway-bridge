7b8e750b9aca0f9ec5c3776eb0461dbdf7a0d3b8
Mamaev [FancaR] Alexandr <fancarster@gmail.com>
2021-06-18 11:36:08 +0300

    added I/O stats for interfaces in gw-statistics

--- internal/metadata/host.go
+++ internal/metadata/host.go
@@ -8,6 +8,7 @@ import (
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/net"
+	log "github.com/sirupsen/logrus"
 	"strconv"
 	"strings"
 	// "time"
@@ -26,6 +27,8 @@ func hostMetrics(conf config.Config) (map[string]string, error) {
 	vmStat, err := mem.VirtualMemory()
 	hostStat, err := host.Info()        // host or machine kernel, uptime, platform Info
 	interfStat, err := net.Interfaces() // get interfaces MAC/hardware address
+	ioStat, err := net.IOCounters(true) // get I/O counters
+
 	if err != nil {
 		return nil, err
 	}
@@ -36,30 +39,49 @@ func hostMetrics(conf config.Config) (map[string]string, error) {
 	result["ram"] = strconv.FormatFloat(vmStat.UsedPercent, 'f', 2, 64)
 	result["uptime"] = strconv.FormatUint(hostStat.Uptime, 10)
 
+	ioMap := make(map[string]net.IOCountersStat)
+	for _, io := range ioStat {
+		ioMap[io.Name] = io
+	}
+
 	for _, interf := range interfStat {
+		iface := ""
+
 		switch strings.ToLower(interf.Name) {
-
 		case ethName:
-			for k, v := range ifaceMetrics(interf, "eth") {
-				result[k] = v
-			}
-
+			iface = "eth"
 		case wifiName:
-			for k, v := range ifaceMetrics(interf, "wlan") {
-				result[k] = v
-			}
-
+			iface = "wlan"
 		case lteName:
-			for k, v := range ifaceMetrics(interf, "lte") {
-				result[k] = v
-			}
-
+			iface = "lte"
 		case vpnName:
-			for k, v := range ifaceMetrics(interf, "vpn") {
-				result[k] = v
-			}
+			iface = "vpn"
+		default:
+			log.WithField("iface", interf.Name).Debug("the interface name wasn't mentioned in cfg. Skipped")
+			continue
+		}
+
+		for k, v := range ifaceMetrics(interf, iface) {
+			result[k] = v
+		}
+
+		if (ioMap[interf.Name] == net.IOCountersStat{}) {
+			log.WithFields(log.Fields{
+				"iface": interf.Name,
+			}).Debug("no I/O stat for the interface")
+
+			continue
+		}
+		for k, v := range netIOMetrics(ioMap[interf.Name], iface) {
+			result[k] = v
 		}
 	}
+
+	fields := log.Fields{}
+	for k, v := range result {
+		fields[k] = v
+	}
+	log.WithFields(fields).Debug("host metrics prepeared")
 
 	return result, nil
 }
@@ -86,5 +108,23 @@ func ifaceMetrics(interf net.InterfaceStat, ifaceName string) map[string]string 
 	if len(interf.Flags) > 0 {
 		result[fmt.Sprintf("%s_flags", ifaceName)] = strings.Join(interf.Flags, ", ")
 	}
+	return result
+}
+
+// makes key-value pairs of I/O counters for interfaces we need to check
+func netIOMetrics(interf net.IOCountersStat, ifaceName string) map[string]string {
+	result := make(map[string]string)
+
+	result[fmt.Sprintf("%s_bytes_sent", ifaceName)] = strconv.FormatUint(interf.BytesSent, 10)
+	result[fmt.Sprintf("%s_bytes_recv", ifaceName)] = strconv.FormatUint(interf.BytesRecv, 10)
+	result[fmt.Sprintf("%s_packets_sent", ifaceName)] = strconv.FormatUint(interf.PacketsSent, 10)
+	result[fmt.Sprintf("%s_packets_recv", ifaceName)] = strconv.FormatUint(interf.PacketsRecv, 10)
+	result[fmt.Sprintf("%s_errin", ifaceName)] = strconv.FormatUint(interf.Errin, 10)
+	result[fmt.Sprintf("%s_errout", ifaceName)] = strconv.FormatUint(interf.Errout, 10)
+	result[fmt.Sprintf("%s_dropin", ifaceName)] = strconv.FormatUint(interf.Dropin, 10)
+	result[fmt.Sprintf("%s_dropout", ifaceName)] = strconv.FormatUint(interf.Dropout, 10)
+	result[fmt.Sprintf("%s_fifoin", ifaceName)] = strconv.FormatUint(interf.Fifoin, 10)
+	result[fmt.Sprintf("%s_fifoout", ifaceName)] = strconv.FormatUint(interf.Fifoout, 10)
+
 	return result
 }
